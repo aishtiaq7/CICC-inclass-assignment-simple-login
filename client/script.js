9adea6624a3646036b4136c5b465103339aa31cc
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-04-04 12:01:34 -0700

    update

--- client/script.js
+++ client/script.js
@@ -6,16 +6,46 @@ document.getElementById("loginButton").addEventListener("click", function () {
   // endpoint to call: http://localhost:3000/login
   // handle response success/error accordingly
   // TIP: you can call ->  alert('msg') <-  to open a pop up to show success/failure
+
+  const url  = "http://localhost:3000/login";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email, 
+      password: password
+    })
+  })
+  .then((resp)=> resp.json())
+  .then((data)=>{
+    console.log('in client:', data);
+    alert(data.message);
+  })
+
 });
 
-document.getElementById("registerButton")
+document
+  .getElementById("registerButton")
   .addEventListener("click", function () {
     const name = document.getElementById("registerName").value;
     const email = document.getElementById("registerEmail").value;
     const password = document.getElementById("registerPassword").value;
 
-    // make a fetch call here
-    // endpoint to call: http://localhost:3000/register
-    // handle response success/error accordingly
-    // TIP: you can call ->  alert('msg') <-  to open a pop up to show success/failure
+    console.log(`name: ${name} email: ${email}, password: ${password} , `);
+    const url = "http://localhost:3000/register";
+    fetch("http://localhost:3000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
+    .then(response => response.json())
+    .then((data)=>{
+      console.log('this data: ', data);
+      alert(data.message)
+    })
+
   });
